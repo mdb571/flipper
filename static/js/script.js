ea2c08028297a217c66ef0efd3758cf359bcfc61
mdb571 <r.mohammedbilal@gmail.com>
2021-01-03 01:36:22 +0530

    animations

--- static/js/script.js
+++ static/js/script.js
@@ -21,3 +21,15 @@ function Predict(){
         console.log(error);
     })
 }
+
+let second_img = document.querySelector(".second-img");
+
+let rotate2021 = () => {
+  console.log('animate')
+  second_img.style.animationName = "rotate2021";
+  second_img.style.animationDuration = "2s";
+
+  setTimeout(() => {
+    second_img.style.animationName = "";
+  }, 2000);
+}
